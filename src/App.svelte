50b81ce38db080bfa56676aaa9e03d26da81fb37
aoshisen <aoshisen.23@gmail.com>
2025-02-21 11:29:16 +0800

    refactor: 抽离Video 组件

--- src/App.svelte
+++ src/App.svelte
@@ -1,113 +1,36 @@
 <script context="module" lang="ts">
-  import videoJs from "video.js";
-  import "video.js/dist/video-js.css";
-  import "videojs-youtube";
-  import { onMount, onDestroy } from "svelte";
-  import type Player from "video.js/dist/types/player";
-  import { pseudo, rewriteFullScreen } from "./helper/fullscreen";
-  import CustomElement, {
-    COMPONENT_NAME,
-  } from "./lib/CustomElement/CustomElement.svelte";
   import Progress from "./lib/Progress/Progress.svelte";
+  import Video, { type Source } from "./lib/Video/Video.svelte";
 </script>
 
 <script lang="ts">
-  // video 元素
-  let videoElement: HTMLVideoElement | null;
-  // 使用video.js 初始化后的播放器实例
-  let player: Player | null;
-
-  // 初始化播放器
-  const initializePlayer = () => {
-    const sources = [
-      {
-        src: "https://vjs.zencdn.net/v/oceans.mp4",
-        type: "video/mp4",
-      },
-      {
-        type: "video/youtube",
-        src: "https://www.youtube.com/watch?v=xjS6SftYQaQ",
-      },
-    ];
-
-    const videoJsConfig = {
-      controls: true,
-      fluid: true,
-      sources,
-      techOrder: ["youtube"],
-    };
-
-    function playerReady(this: Player) {
-      //添加自定义的元素,并传递自定义的参数
-      this.addChild(COMPONENT_NAME, { ass: "1111" });
-      if (!this.fsApi_.fullscreenEnabled) {
-        //重写fullscreen方法
-        rewriteFullScreen.call(this);
-      }
-    }
-
-    return videoJs(videoElement!, videoJsConfig, playerReady);
-  };
-
-  // 清理播放器资源
-  const cleanupPlayer = () => {
-    if (player) {
-      player.dispose();
-      player = null;
-    }
-  };
-
-  //挂载
-  onMount(() => {
-    player = initializePlayer();
-    player.muted(true);
-  });
-
-  //销毁
-  onDestroy(() => {
-    cleanupPlayer();
-  });
+  const sources: Source[] = [
+    {
+      src: "https://vjs.zencdn.net/v/oceans.mp4",
+      type: "video/mp4",
+    },
+    {
+      type: "video/youtube",
+      src: "https://www.youtube.com/watch?v=xjS6SftYQaQ",
+    },
+  ];
 </script>
 
-<div
-  class="video-container"
-  class:pseudo-fullscreen={$pseudo && player?.isFullscreen()}
->
-  <video bind:this={videoElement} class="video-js" playsinline>
-    <track kind="captions" />
-    <p class="vjs-no-js">
-      要查看此视频，请启用 JavaScript，并考虑升级到支持 HTML5 视频的浏览器
-    </p>
-  </video>
-  <CustomElement />
-</div>
-
-<div class="progress">
-  <Progress value={20}  />
+<div class="container">
+  <div class="video">
+    <Video {sources} />
+  </div>
+  <div class="progress">
+    <Progress value={2} />
+  </div>
 </div>
 
 <style>
-  .video-container {
-    width: 100%;
-    max-width: 800px;
-    margin: 0 auto;
-    position: relative;
+  .container {
   }
-
-  .video-container.pseudo-fullscreen {
-    position: fixed !important;
-    width: 100% !important;
-    height: 100% !important;
-    box-sizing: border-box !important;
-    min-width: 0px !important;
-    max-width: none !important;
-    min-height: 0px !important;
-    max-height: none !important;
-    transform: none !important;
-    inset: 0px !important;
-    margin: 0px !important;
+  .container .video {
   }
-  .progress {
+  .container .progress {
     margin-top: 10px;
   }
 </style>
